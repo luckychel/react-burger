5c725f1f47e1f0c8272a30d12317d074a1849e98
Alexander Kukoba <luckychel@gmail.com>
2024-06-10 18:40:03 +0300

    refactoring

--- src/services/reducers/burger.test.js
+++ src/services/reducers/burger.test.js
@@ -10,6 +10,36 @@ const initialState = {
     orderNumber: 0
   };
 
+const bun = {
+  "_id":"60666c42cc7b410027a1a9b1",
+  "name":"Краторная булка N-200i",
+  "type":"bun",
+  "proteins":80,
+  "fat":24,
+  "carbohydrates":53,
+  "calories":420,
+  "price":1255,
+  "image":"https://code.s3.yandex.net/react/code/bun-02.png",
+  "image_mobile":"https://code.s3.yandex.net/react/code/bun-02-mobile.png",
+  "image_large":"https://code.s3.yandex.net/react/code/bun-02-large.png",
+  "__v":0,
+};
+
+const main = {
+  "_id":"60666c42cc7b410027a1a9b5",
+  "name":"Говяжий метеорит (отбивная)",
+  "type":"main",
+  "proteins":800,
+  "fat":800,
+  "carbohydrates":300,
+  "calories":2674,
+  "price":3000,
+  "image":"https://code.s3.yandex.net/react/code/meat-04.png",
+  "image_mobile":"https://code.s3.yandex.net/react/code/meat-04-mobile.png",
+  "image_large":"https://code.s3.yandex.net/react/code/meat-04-large.png",
+  "__v":0
+};
+
 describe('burger reducer', () => {
   test('should return the initial state', () => {
     expect(burgerReducer(undefined, {})).toEqual(initialState)
@@ -20,39 +50,13 @@ describe('burger reducer', () => {
        burgerReducer(initialState, {
         type: types.ADD_INGREDIENT_TO_BURGER,
         payload: { 
-            item: {
-                "_id":"60666c42cc7b410027a1a9b1",
-                "name":"Краторная булка N-200i",
-                "type":"bun",
-                "proteins":80,
-                "fat":24,
-                "carbohydrates":53,
-                "calories":420,
-                "price":1255,
-                "image":"https://code.s3.yandex.net/react/code/bun-02.png",
-                "image_mobile":"https://code.s3.yandex.net/react/code/bun-02-mobile.png",
-                "image_large":"https://code.s3.yandex.net/react/code/bun-02-large.png",
-                "__v":0,
-            },
+            item: bun,
             ingredientType: "bun"
         }
       })
     ).toEqual({
         ...initialState,
-        bun: {
-            "_id":"60666c42cc7b410027a1a9b1",
-            "name":"Краторная булка N-200i",
-            "type":"bun",
-            "proteins":80,
-            "fat":24,
-            "carbohydrates":53,
-            "calories":420,
-            "price":1255,
-            "image":"https://code.s3.yandex.net/react/code/bun-02.png",
-            "image_mobile":"https://code.s3.yandex.net/react/code/bun-02-mobile.png",
-            "image_large":"https://code.s3.yandex.net/react/code/bun-02-large.png",
-            "__v":0,
-        }
+        bun: bun
       }
     )
 
@@ -60,39 +64,13 @@ describe('burger reducer', () => {
         burgerReducer(initialState, {
          type: types.ADD_INGREDIENT_TO_BURGER,
          payload: { 
-             item: {
-                "_id":"60666c42cc7b410027a1a9b5",
-                "name":"Говяжий метеорит (отбивная)",
-                "type":"main",
-                "proteins":800,
-                "fat":800,
-                "carbohydrates":300,
-                "calories":2674,
-                "price":3000,
-                "image":"https://code.s3.yandex.net/react/code/meat-04.png",
-                "image_mobile":"https://code.s3.yandex.net/react/code/meat-04-mobile.png",
-                "image_large":"https://code.s3.yandex.net/react/code/meat-04-large.png",
-                "__v":0
-             },
+             item: main,
              ingredientType: "main"
          }
        })
      ).toEqual({
         ...initialState,
-        burgerIngredients: [{
-            "_id":"60666c42cc7b410027a1a9b5",
-            "name":"Говяжий метеорит (отбивная)",
-            "type":"main",
-            "proteins":800,
-            "fat":800,
-            "carbohydrates":300,
-            "calories":2674,
-            "price":3000,
-            "image":"https://code.s3.yandex.net/react/code/meat-04.png",
-            "image_mobile":"https://code.s3.yandex.net/react/code/meat-04-mobile.png",
-            "image_large":"https://code.s3.yandex.net/react/code/meat-04-large.png",
-            "__v":0
-         }]
+        burgerIngredients: [main]
       })
   })
 })
